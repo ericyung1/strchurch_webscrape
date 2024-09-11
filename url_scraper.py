4c728bbc6ffc37227b4f3c88338089515fb7a315
Eric Yung <eyung@wustl.edu>
2024-09-11 02:14:55 -0500

    updated web url scraper

--- url_scraper.py
+++ url_scraper.py
@@ -3,9 +3,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import time
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+df = pd.read_csv('churches_cleaned.csv')
+df['URL'] = ''
+
+for index, row in df.iterrows():
+    address = row['Address']
+    driver.get("https://www.google.com/")
+
+    search_box = driver.find_element(By.NAME, "q")
+    search_box.send_keys(address)
+    search_box.submit()
+
+    time.sleep(1)
+
+    try:
+        first_link = driver.find_element(By.XPATH, "//div[@class='g']//a").get_attribute("href")
+        df.at[index, 'URL'] = first_link
+    except Exception as e:
+        print(f"Error on address {address}: {e}")
+        df.at[index, 'URL'] = "N/A"
+
+df.to_csv('churches_cleaned.csv', index=False)
